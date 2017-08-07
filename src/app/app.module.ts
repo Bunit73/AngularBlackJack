cf3174e75a8958c1f3237ef0af368f23ac83c5bb
Bunit73 <ford2bm@gmail.com>
2017-08-07 11:33:28 -0400

    Stubbed out control panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ControlPanelComponent } from './control-panel/control-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlPanelComponent
   ],
   imports: [
     BrowserModule
